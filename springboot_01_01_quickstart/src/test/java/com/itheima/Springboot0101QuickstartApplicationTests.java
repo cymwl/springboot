849bb43a3e1f3f4e6738594633381beb793a50cb
chenyiming <17628285006@163.com>
2023-08-30 09:58:13 +0800

    hot-fix commit

--- springboot_01_01_quickstart/src/test/java/com/itheima/Springboot0101QuickstartApplicationTests.java
+++ springboot_01_01_quickstart/src/test/java/com/itheima/Springboot0101QuickstartApplicationTests.java
@@ -13,6 +13,8 @@ class Springboot0101QuickstartApplicationTests {
         long id;
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
     }
 
 }
